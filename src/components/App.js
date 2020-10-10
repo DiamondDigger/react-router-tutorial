f5e4751b8e77e48cde427dfefa6026ce011d9fc5
naglec13 <naglec13@gmail.com>
2020-10-10 19:38:20 +0300

    introduced BrowserRouter, Route and Switch

--- src/components/App.js
+++ src/components/App.js
@@ -3,15 +3,27 @@ import '../App.css';
 import Nav from './Nav';
 import About from './About';
 import Shop from './Shop';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <Nav/>
-      <About/>
-      <Shop/>
-    </div>
+    <Router>
+      <div className="App">
+        <Nav/>
+        <Switch>
+          <Route path='/' exact component={Home} />
+          <Route path='/about' component={About} />
+          <Route path='/shop' component={Shop} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
+
+const Home = () => (
+  <div>
+    <h1>Home page</h1>
+  </div>
+)
 
 export default App;
